8ac515f123fd9c368d67efaf676d6dd595a78179
Rick <rickatherton316@gmail.com>
2022-04-05 11:35:05 +0000

    Create map.js and import code for Google Maps to the Interests page

---
+++ assets/js/maps.js
@@ -0,0 +1,42 @@
+// import { MarkerClusterer } from "@googlemaps/markerclusterer";
+
+// const markerCluster = new MarkerClusterer({ map, markers });
+
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: { lat: 52.355518, lng: -1.174320 },
+  });
+  const infoWindow = new google.maps.InfoWindow({
+    content: "",
+    disableAutoPan: true,
+  });
+  // Create an array of alphabetical characters used to label the markers.
+  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // Add some markers to the map.
+  const markers = locations.map((position, i) => {
+    const label = labels[i % labels.length];
+    const marker = new google.maps.Marker({
+      position,
+      label,
+    });
+
+    // markers can only be keyboard focusable when they have click listeners
+    // open info window when marker is clicked
+    marker.addListener("click", () => {
+      infoWindow.setContent(label);
+      infoWindow.open(map, marker);
+    });
+    return marker;
+  });
+
+  // Add a marker clusterer to manage the markers.
+  new MarkerClusterer({ markers, map });
+}
+
+const locations = [
+  { lat: 53.480759, lng: -2.242631 },
+];
+
+// export { initMap };
